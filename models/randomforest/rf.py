c3c8ace993b30fa287776b8ecb319705808c5476
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2018-11-03 21:50:10 +0600

    latest linreg implementation updated to rf

--- models/randomforest/rf.py
+++ models/randomforest/rf.py
@@ -2,9 +2,13 @@ import sys
 import os
 import random
 import numpy as np
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import RobustScaler
 from scipy.stats import pearsonr
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 from Bio.PDB import Polypeptide
 
 def aa_to_index(aa):
@@ -17,6 +21,57 @@ def aa_to_index(aa):
     else:
         return 20
 
+def get_pccs_and_mses(protein_seqs, protein_bvals, indices, ws, clf):
+    pccs = []
+    mses = []
+    for i in indices:
+        X = []
+        for j in range(ws, len(protein_seqs[i]) - ws):
+            X.append(np.array(protein_seqs[i][j - ws:j + ws + 1]))
+        X = np.vstack(X)
+        #X = oh.transform(X)
+        y_pred = clf.predict(X)
+        pccs.append(pearsonr(y_pred, protein_bvals[i])[0])
+        mses.append(mean_squared_error(y_pred, protein_bvals[i]))
+
+    pccs = np.array(pccs)
+    mses = np.array(mses)
+    return pccs, mses
+
+def get_stats_on_pccs_and_mses(pccs, mses, prefix, ws, indices, protein_seqs, protein_bvals, protein_list):
+    lens = [len(protein_seqs[i]) - 2 * ws for i in indices]
+    bvals_mean = [protein_bvals[i].mean() for i in indices]
+    plt.hist(pccs, density=True, range=(-0.5,1.0), bins=50)
+    plt.savefig(prefix + '-pcc-hist-{:02d}.png'.format(ws))
+    plt.close()
+    plt.hist(mses, density=True, range=(0.0, 5.0), bins=50)
+    plt.savefig(prefix + '-mse-hist-{:02d}.png'.format(ws))
+    plt.close()
+    print(prefix.upper())
+    print("Mean PCC: {} +- {}".format(pccs.mean(), 3 * pccs.std()))
+    print("PCC: Min: {} Max: {}".format(pccs.min(), pccs.max()))
+    print("Mean MSE: {} +- {}".format(mses.mean(), 3 * mses.std()))
+    print("MSE: Min: {} Max: {}".format(mses.min(), mses.max()))
+ 
+    indices_sorted = sorted(enumerate(indices), key=lambda t: mses[t[0]])
+    indices_sorted = [(protein_list[t[1]],len(protein_seqs[t[1]]) - 2 * ws) for t in indices_sorted]
+    print("MSE:\nArgMin: {}\nArgMax: {}".format(indices_sorted[:10], list(reversed(indices_sorted[-10:]))))
+
+    indices_sorted = sorted(enumerate(indices), key=lambda t: pccs[t[0]])
+    indices_sorted = [(protein_list[t[1]],len(protein_seqs[t[1]]) - 2 * ws) for t in indices_sorted]
+    print("PCC:\nArgMin: {}\nArgMax: {}".format(indices_sorted[:10], list(reversed(indices_sorted[-10:]))))
+
+
+    clf = LinearRegression()
+    clf.fit(mses.reshape((-1, 1)), lens)
+    print("MSE vs Length correlation: {}".format(clf.score(mses.reshape((-1,1)), lens)))
+    clf.fit(pccs.reshape((-1, 1)), lens)
+    print("PCC vs Length correlation: {}".format(clf.score(pccs.reshape((-1,1)), lens)))
+    clf.fit(mses.reshape((-1, 1)), bvals_mean)
+    print("MSE vs b-val mean correlation: {}".format(clf.score(mses.reshape((-1,1)), bvals_mean)))
+    clf.fit(pccs.reshape((-1, 1)), bvals_mean)
+    print("PCC vs b-val mean correlation: {}".format(clf.score(pccs.reshape((-1,1)), bvals_mean)))
+ 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         print('Usage: python3 linreg.py protein_list input_dir window_size')
@@ -29,8 +84,7 @@ if __name__ == '__main__':
 
     protein_list = []
     with open(protein_list_file) as f:
-        protein_list.extend([l for l in f])
-
+        protein_list.extend([l.strip() for l in f])
 
     indices = list(range(len(protein_list)))
     random.seed(42)
@@ -44,8 +98,13 @@ if __name__ == '__main__':
         protein = protein.strip()
         with open(os.path.join(input_dir, protein)) as f:
             lines = [l.split() for l in f]
-            a = [aa_to_index(l[0]) for l in lines]
+            a = ws * [20]
+            a.extend([aa_to_index(l[0]) for l in lines])
+            a.extend(ws * [20])
             b = [float(l[1]) for l in lines]
+            b = np.array(b)
+            scaler = RobustScaler()
+            b = scaler.fit_transform(b.reshape((-1, 1))).reshape((-1,))
             protein_seqs.append(a)
             protein_bvals.append(b)
 
@@ -55,7 +114,7 @@ if __name__ == '__main__':
     for i in train_indices:
         for j in range(ws, len(protein_seqs[i]) - ws):
             X.append(np.array(protein_seqs[i][j - ws:j + ws + 1]))
-        y.extend(protein_bvals[i][ws:len(protein_bvals[i])-ws])
+        y.extend(protein_bvals[i])
 
     X = np.vstack(X)
     y = np.array(y)
@@ -64,19 +123,12 @@ if __name__ == '__main__':
     #oh.fit(X)
     #X = oh.transform(X)
     print("Converted to numpy array.")
+
     clf = RandomForestRegressor(max_features='sqrt', max_depth=3, n_estimators=10)
     clf.fit(X, y)
-    
     print("Model fit done.")
-    val_pccs = []
-    for i in validation_indices:
-        X = []
-        for j in range(ws, len(protein_seqs[i]) - ws):
-            X.append(np.array(protein_seqs[i][j - ws:j + ws + 1]))
-        X = np.vstack(X)
-        #X = oh.transform(X)
-        y_pred = clf.predict(X)
-        val_pccs.append(pearsonr(y_pred, protein_bvals[i][ws:len(protein_bvals[i])-ws])[0])
-
-    print("Validation Mean PCC: {}".format(sum(val_pccs) / len(val_pccs)))
+    train_pccs, train_mses = get_pccs_and_mses(protein_seqs, protein_bvals, train_indices, ws, clf)
+    val_pccs, val_mses = get_pccs_and_mses(protein_seqs, protein_bvals, validation_indices, ws, clf)
+    get_stats_on_pccs_and_mses(train_pccs, train_mses, 'train', ws, train_indices, protein_seqs, protein_bvals, protein_list)
+    get_stats_on_pccs_and_mses(val_pccs, val_mses, 'val', ws, validation_indices, protein_seqs, protein_bvals, protein_list)
 
